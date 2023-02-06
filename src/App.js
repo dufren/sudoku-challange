ba45fc0a8cbb7576501702555b2a39534d9975b0
ozgur <ozgurcokckn@gmail.com>
2023-02-06 23:33:19 +0300

    axios fetch

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
-import { useState, useEffect } from "react"
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [puzzle, setPuzzle] = useState([]);
+  const [solution, setSolution] = useState([]);
 
-  const [api, setApi] = useState()
-  const [puzzle, setPuzzle] = useState([])
-  const [solution, setSolution] = useState([])
+  const handleFetchPuzzle = () => {
+    axios
+      .get(
+        "https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value}}}"
+      )
+      .then((res) => {
+        const data = res.data;
+        setPuzzle(data.newboard.grids[0].value);
+      });
+  };
 
-  useEffect(() => {
-    fetch("https://sudoku-api.vercel.app/api/dosuku")
-      .then(res => res.json())
-      .then(res => setApi(res))
-  }, [])
+  const handleFetchSolution = () => {
+    axios
+      .get(
+        "https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{solution}}}"
+      )
+      .then((res) => {
+        const data = res.data;
+        setSolution(data.newboard.grids[0].solution);
+      });
+  };
 
-  const handleSolve = async () => {
-    const solution = await api?.newboard.grids[0].solution
-    setSolution(solution)
-  }
-
-  const handleValue = async () => {
-    const value = await api?.newboard.grids[0].value
-    setPuzzle(value)
-  }
-
-  const handleCol = () => {
-
-  }
+  const handleCol = () => {};
 
   return (
-    <div>
+    <div className="app">
       <div className="buttons">
-        <button onClick={handleValue}>new table</button>
-        <button onClick={handleSolve}>solve</button>
+        <button onClick={handleFetchPuzzle}>new table</button>
+        <button onClick={handleFetchSolution}>solve</button>
       </div>
 
       <div className="grid">
-        {
-          puzzle.map((row, rowIdx) => (
-            <div key={rowIdx}>
-              {row.map((col, colIdx) => (
-                <input key={colIdx} type="text" value={col} onChange={handleCol} />
-              ))}
-            </div>
-          ))
-        }
+        {puzzle.map((row, rowIdx) => (
+          <div key={rowIdx} className="row">
+            {row.map((col, colIdx) => (
+              <div key={colIdx} className="col">
+                <input type="text" value={col} onChange={handleCol} />
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
