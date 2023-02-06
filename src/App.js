2322363590c5dad363bf0d70700a3c350bc3c773
Özgür ÇOKÇEKEN <ozgur.cokceken@istinye.edu.tr>
2023-02-06 15:47:49 +0300

    fetching api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
 
 function App() {
+  const [api, setApi] = useState()
+  const [puzzle, setPuzzle] = useState([])
+
+  useEffect(() => {
+    fetch("https://sudoku-api.vercel.app/api/dosuku")
+      .then(res => res.json())
+      .then(res => setApi(res))
+  }, [])
+
+  const handleSolve = async () => {
+    const solution = await api?.newboard.grids[0].solution
+  }
+
+  const handleValue = async () => {
+    const value = await api?.newboard.grids[0].value
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
