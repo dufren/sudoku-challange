d882539661697368d6df1cd65d38ed054b22ea55
ozgur <ozgurcokckn@gmail.com>
2023-02-08 00:05:54 +0300

    fetch data right way

--- src/App.js
+++ src/App.js
@@ -16,27 +16,18 @@ const initialState = [
 function App() {
   const [puzzle, setPuzzle] = useState(initialState);
   const [solution, setSolution] = useState([]);
+  const [visible, setVisible] = useState(false);
 
   const handleFetchPuzzle = () => {
-    axios
-      .get(
-        "https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value}}}"
-      )
-      .then((res) => {
-        const data = res.data;
-        setPuzzle(data.newboard.grids[0].value);
-      });
+    axios.get("https://sudoku-api.vercel.app/api/dosuku").then((res) => {
+      const data = res.data;
+      setPuzzle(data.newboard.grids[0].value);
+      setSolution(data.newboard.grids[0].solution);
+    });
   };
 
-  const handleFetchSolution = () => {
-    axios
-      .get(
-        "https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{solution}}}"
-      )
-      .then((res) => {
-        const data = res.data;
-        setSolution(data.newboard.grids[0].solution);
-      });
+  const handleSolution = () => {
+    setVisible(!visible);
   };
 
   const handleCol = (rowIdx, colIdx, value) => {
@@ -49,23 +40,41 @@ function App() {
     <div className="app">
       <div className="buttons">
         <button onClick={handleFetchPuzzle}>new table</button>
-        <button onClick={handleFetchSolution}>solve</button>
+        <button onClick={handleSolution}>solve</button>
       </div>
-
-      <div className="grid">
-        {puzzle.map((row, rowIdx) => (
-          <div key={rowIdx} className="row">
-            {row.map((col, colIdx) => (
-              <div key={colIdx} className="col">
-                <input
-                  type="text"
-                  value={col}
-                  onChange={(e) => handleCol(rowIdx, colIdx, e.target.value)}
-                />
+      <div className="main">
+        <div>
+          <h1>sudoku</h1>
+          <div className="grid">
+            {puzzle?.map((row, rowIdx) => (
+              <div key={rowIdx} className="row">
+                {row.map((col, colIdx) => (
+                  <div key={colIdx} className="col">
+                    <input type="text" value={col} onChange={handleCol} />
+                  </div>
+                ))}
               </div>
             ))}
           </div>
-        ))}
+        </div>
+
+        <div>
+          <h1>solution</h1>
+          {!visible && <h1>press solve to solution</h1>}
+          {visible && (
+            <div className="grid-solution">
+              {solution?.map((row, rowIdx) => (
+                <div key={rowIdx} className="row">
+                  {row.map((col, colIdx) => (
+                    <div key={colIdx} className="col">
+                      <input type="text" value={col} onChange={handleCol} />
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
